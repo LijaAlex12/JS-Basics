3a275854f8665e078a745123bcd92d721d14ec45
Lija Alex <48295613+LijaAlex12@users.noreply.github.com>
2020-03-22 03:09:08 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -14,5 +14,7 @@ function calculator(num1,num2,operator){
   return operator(num1,num2);
 }
 
+
+#here calculator is the higher order function and operator is the call back function
 console.log(calculator(1,2,add));
 console.log(calculator(1,2,multiply));
