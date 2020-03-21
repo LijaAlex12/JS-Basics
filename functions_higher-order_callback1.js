94dfc3fa8bbef6dfc75a2d462d7a1306a0ea475d
Lija Alex <48295613+LijaAlex12@users.noreply.github.com>
2020-03-22 03:31:02 +0530

    functions_higher-order_callback1.js

---
+++ functions_higher-order_callback1.js
@@ -0,0 +1,18 @@
+//when we call a function we can set as its arguments the ordinary stuffs like values as well functions that performs some action
+//for example:
+  document.querySelector('h1').addEventListener("click",function(){
+    alert('I got clicked');
+  });
+  //where addEventListener is the higher order function and the function passed inside is called callback function
+  
+  
+  
+  //the other way round is:
+  document.querySelector('h1').addEventListener("click",function_name) // no paranthesis coz for passing an input that is to
+    alert('I got clicked');                                                                    //be called back 
+    });                                                                    //at a later time not when the event listener is added
+                                                                        //but on click
+  function function_name(){
+    alert('hello');
+  }
+  
