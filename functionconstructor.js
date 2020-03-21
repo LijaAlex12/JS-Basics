07014b58fcfd0a4cdda29f606ffc9f22f4e089e3
Lija Alex <48295613+LijaAlex12@users.noreply.github.com>
2020-03-21 22:54:28 +0530

    functionconstructor.js

---
+++ functionconstructor.js
@@ -0,0 +1,22 @@
+// var houseKeeper={
+//     yearOfExperience:12,
+//     name:"Jane",
+//     cleaningRepertoire:['bathroom','lobby','bedroom']
+
+
+// }
+
+
+//HouseKeeper constructor function
+
+//creating  object
+    // var houseKeeper1=new HouseKeeper(1,'lija',['living','dining']) ;
+
+function HouseKeeper(yearOfExperience,name,cleaningRepertoire){
+    this.yearOfExperience=yearOfExperience;
+    this.name=name;
+    this.cleaningRepertoire=cleaningRepertoire;
+}
+var house1=new HouseKeeper(1,'ndl',['dhak','afhkg']);
+
+console.log(house1);
