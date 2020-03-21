2771fe4eb4b632986d470eea6878869595ae17da
Lija Alex <48295613+LijaAlex12@users.noreply.github.com>
2020-03-21 23:09:57 +0530

    love score using Math.random

---
+++ lovescore.js
@@ -0,0 +1,17 @@
+// function bmiCalculator(weight,height,bmi){
+//      bmi=Math.floor(weight/(height*height));
+// //     console.log(bmi);
+//      return bmi;
+//     return;
+
+    
+// }
+// var bmi;
+// bmi=bmiCalculator(5,1,bmi);
+// console.log(bmi);
+
+
+prompt('ur name');
+prompt('their name');
+var lovescore=Math.floor(Math.random()*100)+1;
+console.log(lovescore);
